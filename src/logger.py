c027964cfc7beb0cd201ddcd0efae15c76e4d284
Thomas Kemkemian <thomas.kemkemian@gmail.com>
2025-02-10 01:34:27 +0100

    Chore: update logger configuration

--- src/logger.py
+++ src/logger.py
@@ -4,4 +4,8 @@ from loguru import logger
 
 
 logger.remove(0)
-logger.add(sys.stdout)
+logger.add(sys.stdout, level="INFO")
+logger.add(sys.stdout, level="SUCCESS")
+logger.add(sys.stderr, level="WARNING")
+logger.add(sys.stderr, level="ERROR")
+logger.add(sys.stderr, level="CRITICAL")
